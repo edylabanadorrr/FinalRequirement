ba022dfa646c5b0b5e81148467487815fa1c0b5a
Edylbert Abanador <abanadoredylbert@gmail.com>
2023-07-04 11:33:20 +0800

    0704

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: '', redirectTo: 'landing', pathMatch: 'full'},
+  { path: 'landing', loadChildren: () => import('./landing/landing.module').then( m => m.LandingPageModule)},
+
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   {
     path: 'login',
