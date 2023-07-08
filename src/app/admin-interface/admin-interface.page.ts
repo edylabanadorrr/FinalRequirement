a0e8bb63c29d27c9d8e8f5b53ca388d73fa141f8
Edylbert Abanador <abanadoredylbert@gmail.com>
2023-07-08 18:57:49 +0800

    search

--- src/app/admin-interface/admin-interface.page.ts
+++ src/app/admin-interface/admin-interface.page.ts
@@ -1,25 +1,49 @@
 import { Component, OnInit } from '@angular/core';
-import { Consumer, ConsumerService } from '../consumer.service';
 import { HttpClient } from '@angular/common/http';
+
+interface Item {
+  firstName: string;
+  lastName: string;
+  accountNumber: string;
+  userName: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-admin-interface',
   templateUrl: './admin-interface.page.html',
   styleUrls: ['./admin-interface.page.scss'],
 })
-
 export class AdminInterfacePage implements OnInit {
-  jsonData: any;
+  jsonData: Item[];
+  filteredItems: Item[];
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
     this.loadData();
   }
 
   loadData() {
-    this.http.get('assets/super-admin-data.json').subscribe((data) => {
+    this.http.get<Item[]>('assets/super-admin-data.json').subscribe((data) => {
       this.jsonData = data;
+      this.filteredItems = this.jsonData;
     });
   }
 
-  ngOnInit() {}
+  searchItems(searchTerm: string) {
+    if (searchTerm && searchTerm.trim() !== '') {
+      this.filteredItems = this.jsonData.filter((item) => {
+        return (
+          item.firstName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          item.lastName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          item.accountNumber.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          item.userName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          item.password.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+      });
+    } else {
+      this.filteredItems = this.jsonData;
+    }
+  }
 }
