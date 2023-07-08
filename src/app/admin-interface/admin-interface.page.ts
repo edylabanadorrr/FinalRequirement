83bdf134f27eb187653be227f6ef0e3f92eb0618
Edylbert Abanador <abanadoredylbert@gmail.com>
2023-07-08 19:21:04 +0800

    Search bar functioning in super admin

--- src/app/admin-interface/admin-interface.page.ts
+++ src/app/admin-interface/admin-interface.page.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 interface Item {
   firstName: string;
   lastName: string;
-  accountNumber: string;
+  accountNumber: number;
   userName: string;
   password: string;
 }
@@ -19,7 +19,6 @@ export class AdminInterfacePage implements OnInit {
   filteredItems: Item[];
 
   constructor(private http: HttpClient) {}
-
   ngOnInit() {
     this.loadData();
   }
@@ -28,18 +27,20 @@ export class AdminInterfacePage implements OnInit {
     this.http.get<Item[]>('assets/super-admin-data.json').subscribe((data) => {
       this.jsonData = data;
       this.filteredItems = this.jsonData;
+      console.log(this.jsonData);
     });
   }
 
   searchItems(searchTerm: string) {
     if (searchTerm && searchTerm.trim() !== '') {
       this.filteredItems = this.jsonData.filter((item) => {
+        const lowerCaseSearchTerm = searchTerm.toLowerCase();
         return (
-          item.firstName.toLowerCase().includes(searchTerm.toLowerCase()) ||
-          item.lastName.toLowerCase().includes(searchTerm.toLowerCase()) ||
-          item.accountNumber.toLowerCase().includes(searchTerm.toLowerCase()) ||
-          item.userName.toLowerCase().includes(searchTerm.toLowerCase()) ||
-          item.password.toLowerCase().includes(searchTerm.toLowerCase())
+          item.firstName.toLowerCase().includes(lowerCaseSearchTerm) ||
+          item.lastName.toLowerCase().includes(lowerCaseSearchTerm) ||
+          item.accountNumber.toString().toLowerCase().includes(lowerCaseSearchTerm) ||
+          item.userName.toLowerCase().includes(lowerCaseSearchTerm) ||
+          item.password.toLowerCase().includes(lowerCaseSearchTerm)
         );
       });
     } else {
