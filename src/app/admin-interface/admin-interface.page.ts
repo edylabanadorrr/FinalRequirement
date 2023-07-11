3c25ff669c79395583f78e3a43a328d4e75272c2
edylabanadorrr <abanadoredylbert@gmail.com>
2023-07-11 17:18:57 +0800

    Try xml

--- src/app/admin-interface/admin-interface.page.ts
+++ src/app/admin-interface/admin-interface.page.ts
@@ -2,9 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 interface Consumer {
+  id: number;
   firstName: string;
   lastName: string;
-  accountNumber: number;
+  accountNumber: string;
   username: string;
   password: string;
 }
@@ -23,7 +24,22 @@ export class AdminInterfacePage implements OnInit {
   ngOnInit() {
     this.loadData();
   }
-
+  populateConsumerData(consumers: any) {
+    const selectedConsumer = this.consumers.find(c => c.id === consumers.id);
+    if (selectedConsumer) {
+      const firstNameInput = document.getElementById('firstName') as HTMLInputElement;
+      const lastNameInput = document.getElementById('lastName') as HTMLInputElement;
+      const accountNumberInput = document.getElementById('accountNumber') as HTMLInputElement;
+      const usernameInput = document.getElementById('username') as HTMLInputElement;
+      const passwordInput = document.getElementById('password') as HTMLInputElement;
+  
+      firstNameInput.value = selectedConsumer.firstName;
+      lastNameInput.value = selectedConsumer.lastName;
+      accountNumberInput.value = selectedConsumer.accountNumber;
+      usernameInput.value = selectedConsumer.username;
+      passwordInput.value = selectedConsumer.password;
+    }
+  }
   loadData() {
     this.http.get<{ consumers: Consumer[] }>('assets/data/super-admin-data.json').subscribe(
       (data) => {
@@ -53,4 +69,7 @@ export class AdminInterfacePage implements OnInit {
       this.filteredConsumers = this.consumers;
     }
   }
+  
+  
+  
 }
