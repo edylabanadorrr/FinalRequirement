6919b6a85bb9ad83e5b7eca2a5f6f3d22f954228
edylabanadorrr <abanadoredylbert@gmail.com>
2023-07-12 12:21:27 +0800

    12:21pm

--- src/app/admin-interface/admin-interface.page.ts
+++ src/app/admin-interface/admin-interface.page.ts
@@ -49,7 +49,6 @@ export class AdminInterfacePage implements OnInit {
     passwordInput.value = consumer.password;
   }
 
-
   searchItems(searchTerm: string) {
     if (searchTerm && searchTerm.trim() !== '') {
       this.filteredConsumers = this.consumers.filter((consumer) => {
@@ -69,5 +68,51 @@ export class AdminInterfacePage implements OnInit {
   
   maskPassword(password: string): string {
     return '*'.repeat(password.length);
+  }
+  
+  updateConsumerData(consumers: any) {
+    var selectedRow = consumers;
+
+    if (!selectedRow) {
+      alert('Please select a consumer from the table.');
+      return;
+    }
+    const firstNameInput = document.getElementById('firstName') as HTMLInputElement;
+    const lastNameInput = document.getElementById('lastName') as HTMLInputElement;
+    const accountNumberInput = document.getElementById('accountNumber') as HTMLInputElement;
+    const usernameInput = document.getElementById('username') as HTMLInputElement;
+    const passwordInput = document.getElementById('password') as HTMLInputElement;
+
+    firstNameInput.value = consumers.firstName;
+    lastNameInput.value = consumers.lastName;
+    accountNumberInput.value = consumers.accountNumber;
+    usernameInput.value = consumers.username;
+    passwordInput.value = consumers.password;
+    
+    this.http.get<{ consumers: Consumer[] }>('assets/data/super-admin-data.json').subscribe(
+      (data) => {
+        this.consumers = data.consumers;
+        this.filteredConsumers = this.consumers;
+        console.log(this.consumers);
+        this.clearForm();
+      },
+      (error) => {
+        console.error('Error fetching consumer data:', error);
+      }
+    );
+  }
+  
+  clearForm() {
+    const firstNameInput = document.getElementById('firstName') as HTMLInputElement;
+    const lastNameInput = document.getElementById('lastName') as HTMLInputElement;
+    const accountNumberInput = document.getElementById('accountNumber') as HTMLInputElement;
+    const usernameInput = document.getElementById('username') as HTMLInputElement;
+    const passwordInput = document.getElementById('password') as HTMLInputElement;
+
+    firstNameInput.value = '';
+    lastNameInput.value = '';
+    accountNumberInput.value = '';
+    usernameInput.value = '';
+    passwordInput.value = '';
   }
 }
