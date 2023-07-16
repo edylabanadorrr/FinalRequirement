5727bcda1da1b035924ff19b6be1611023f23539
edylabanadorrr <abanadoredylbert@gmail.com>
2023-07-16 23:20:07 +0800

    Background Image - Landing

--- src/app/landing/landing.page.ts
+++ src/app/landing/landing.page.ts
@@ -1,27 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HostListener } from '@angular/core';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.page.html',
   styleUrls: ['./landing.page.scss'],
 })
+
 export class LandingPage implements OnInit {
   isMinimized = false;
   showHeaderContent = true;
 
   constructor(private router: Router) { }
+
   login() {
     this.router.navigate(['/protected-page']);
   }
+
   ngOnInit() {
   }
+
   @HostListener('window:resize', [])
-  onWindowResize() {
-    this.checkScreenSize();
-  }
+    onWindowResize() {
+      this.checkScreenSize();
+    }
+
   private checkScreenSize() { // for showing the menu
     this.isMinimized = window.innerWidth <= 700;
     this.showHeaderContent = window.innerWidth > 700;
